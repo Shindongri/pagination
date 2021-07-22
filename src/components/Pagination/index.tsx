5c1a5682c4cef207322f8d657004725073d70a17
shindongri <tlsehdfl@naver.com>
2021-07-22 10:06:09 +0900

    add use pagination hook

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -3,14 +3,7 @@ import styled from '@emotion/styled/macro';
 import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
 import { AiOutlineEllipsis } from 'react-icons/ai'
 
-interface Props {
-  count: number;
-  page: number;
-  onPageChange: (page: number) => void;
-  disabled?: boolean;
-  siblingCount?: number;
-  boundaryCount?: number;
-}
+import usePagination from '../../hooks/usePagination';
 
 const Navigation = styled.nav``;
 
@@ -47,13 +40,16 @@ const PaginationItemList = styled.ul`
   }
 `;
 
+interface Props {
+  count: number;
+  page: number;
+  onPageChange: (page: number) => void;
+  disabled?: boolean;
+  siblingCount?: number;
+  boundaryCount?: number;
+}
+
 const Pagination: React.FC<Props> = ({ count, page, onPageChange, disabled, siblingCount = 1, boundaryCount  = 1 }) => {
-  const range = (start: number, end: number) => {
-    const length = end - start + 1;
-
-    return Array.from({ length }).map((_, index) => index + start);
-  };
-
   const getLabel = (item: number | string) => {
     if (typeof item === 'number') return item;
     else if (item.indexOf('ellipsis') > -1) return <AiOutlineEllipsis />;
@@ -62,62 +58,15 @@ const Pagination: React.FC<Props> = ({ count, page, onPageChange, disabled, sibl
     else return item;
   }
 
-  const startPage = 1;
-  const endPage = count;
-
-  const startPages = range(startPage, Math.min(boundaryCount, count));
-  const endPages = range(Math.max(count - boundaryCount + 1, boundaryCount + 1), count);
-
-  const siblingsStart = Math.max(
-    Math.min(
-      page + 1 - siblingCount,
-      count - boundaryCount - siblingCount * 2 - 1,
-    ),
-    boundaryCount + 2
-  );
-
-  const siblingsEnd = Math.min(
-    Math.max(
-      page + 1 + siblingCount,
-      boundaryCount + siblingCount * 2 + 2,
-    ),
-    endPages.length > 0 ? endPages[0] - 2 : endPage - 1,
-  );
-
-  const itemList = [
-    'prev',
-    ...startPages,
-    ...(siblingsStart > boundaryCount + 2 ? ['start-ellipsis'] : boundaryCount + 1 < count - boundaryCount ? [boundaryCount + 1] : []),
-    ...range(siblingsStart, siblingsEnd),
-    ...(siblingsEnd < count - boundaryCount - 1 ? ['end-ellipsis'] : count - boundaryCount > boundaryCount ? [count - boundaryCount] : []),
-    ...endPages,
-    'next'
-  ];
-
-  const items = itemList.map((item, index) => (
-    typeof item === 'number' ? {
-        key: index,
-        onClick: () => onPageChange(item - 1),
-        disabled,
-        selected: item - 1 === page,
-        label: getLabel(item),
-      } :
-      {
-        key: index,
-        onClick: () => onPageChange(item === 'next' ? page + 1 : page - 1),
-        disabled: disabled || item.indexOf('ellipsis') > -1 || (item === 'next' ? page >= count - 1 : page - 1 < 0),
-        selected: false,
-        label: getLabel(item),
-      }
-  ));
+  const { items } = usePagination({ count, page, onPageChange, disabled, siblingCount, boundaryCount });
 
   return (
     <Navigation>
       <PaginationItemList>
         {
-          items.map(({ key, disabled, selected, onClick, label }) => (
+          items.map(({ key, disabled, selected, onClick, item }) => (
             <PaginationItem key={key}>
-              <Button disabled={disabled} selected={selected} onClick={onClick}>{label}</Button>
+              <Button disabled={disabled} selected={selected} onClick={onClick}>{getLabel(item)}</Button>
             </PaginationItem>
           ))
         }
